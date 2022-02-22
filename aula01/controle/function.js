cfbe56e8ee76c25039b01e1129a908e3d2085716
Maluzinha16 <maria.nogueira49@etec.sp.gov.br>
2022-02-22 09:14:41 -0300

    commit - sss

---
+++ aula01/controle/function.js
@@ -0,0 +1,10 @@
+$(ducument) .ready(function(){
+
+    //Monitorar o clique em cima do botão btn-send
+    $('.btn-send') .click(function(e){
+        e.preventDefault()
+
+        alert('Você clicou no botão enviar')
+    })
+
+})
